4564b7cb43c809abec96ff5d0cfe72ca008f7ab6
mmishere <mmaajed2@illinois.edu>
2021-05-27 18:08:45 -0500

    added lambda stuff

--- __main__.py
+++ __main__.py
@@ -8,7 +8,6 @@ courier_new = 'Courier New'
 
 options_br = {'bg': 'black', 'fg': 'red'} # black red
 options_bw = {'bg': 'black', 'fg': 'white'} # black white
-options_bb = {'bg': 'black', 'fg': 'black'} # black black
 
 class MainFrame(Frame):
     def __init__(self, container):
@@ -34,12 +33,14 @@ class NewCharacterFrame(Frame):
         self.role = Entry(self, **options_bw, width = 0, font = (courier_new, 12))
         self.role.insert(0, 'Enter Role')
         self.role.pack()
+
         self.npc_str = IntVar(self, value=True)
         self.chara = Radiobutton(self, **options_bw, text = "Player character", font = (courier_new, 12), variable = self.npc_str, value = False)
         self.chara.pack()
         self.npc = Radiobutton(self, **options_bw, text = "NPC", font = (courier_new, 12), variable = self.npc_str, value = True)
         self.npc.pack()
-        self.enter_character = Button(self, **options_bw, text = "Create character", font = (courier_new, 12), command = self.create_character)
+
+        self.enter_character = Button(self, **options_bw, text = "Create character", font = (courier_new, 12), command = lambda: self.create_character)
         self.enter_character.pack()
 
     def create_character():
@@ -76,49 +77,49 @@ class App(Tk):
         # i tried to abstract this bit into a function and it didn't work, so :')
         self.menu_bar.add_command(label = "Main Menu", command = lambda: self.show_frame(MainFrame))
         character_menu = Menu(self.menu_bar, tearoff = 0)
-        character_menu.add_command(label = "All", command = self.show_frame(AllCharactersFrame))
+        character_menu.add_command(label = "All", command = lambda: self.show_frame(AllCharactersFrame))
         character_menu.add_command(label = "New Character", command = lambda: self.show_frame(NewCharacterFrame))
-        character_menu.add_command(label = "NPCs", command = self.show_frame(NPCsFrame))
-        character_menu.add_command(label = "Players", command = self.show_frame(PlayersFrame))
+        character_menu.add_command(label = "NPCs", command = lambda: self.show_frame(NPCsFrame))
+        character_menu.add_command(label = "Players", command = lambda: self.show_frame(PlayersFrame))
 
         self.menu_bar.add_cascade(label = "Characters", menu = character_menu)
 
         gear_menu = Menu(self.menu_bar, tearoff = 0)
-        gear_menu.add_command(label = "All", command = self.show_frame(AllGearFrame))
-        gear_menu.add_command(label = "Communications", command = self.show_frame(CommunicationsFrame))
-        gear_menu.add_command(label = "Data Systems", command = self.show_frame(DataSystemsFrame))
-        gear_menu.add_command(label = "Entertainment", command = self.show_frame(EntertainmentFrame))
-        gear_menu.add_command(label = "Fashion", command = self.show_frame(FashionFrame))
-        gear_menu.add_command(label = "Furnishings", command = self.show_frame(FurnishingsFrame))
-        gear_menu.add_command(label = "Groceries", command = self.show_frame(GroceriesFrame))
-        gear_menu.add_command(label = "Housing", command = self.show_frame(HousingFrame))
-        gear_menu.add_command(label = "Lifestyle", command = self.show_frame(LifestyleFrame))
-        gear_menu.add_command(label = "Medical", command = self.show_frame(MedicalFrame))
-        gear_menu.add_command(label = "Miscellaneous", command = self.show_frame(MiscGearFrame)) # for player-added items with no type
-        gear_menu.add_command(label = "Personal Electronics", command = self.show_frame(PersonalElectronicsFrame))
-        gear_menu.add_command(label = "Security", command = self.show_frame(SecurityFrame))
-        gear_menu.add_command(label = "Surveillance", command = self.show_frame(SurveillanceFrame))
-        gear_menu.add_command(label = "Vehicles", command = self.show_frame(VehiclesFrame))
+        gear_menu.add_command(label = "All", command = lambda: self.show_frame(AllGearFrame))
+        gear_menu.add_command(label = "Communications", command = lambda: self.show_frame(CommunicationsFrame))
+        gear_menu.add_command(label = "Data Systems", command = lambda: self.show_frame(DataSystemsFrame))
+        gear_menu.add_command(label = "Entertainment", command = lambda: self.show_frame(EntertainmentFrame))
+        gear_menu.add_command(label = "Fashion", command = lambda: self.show_frame(FashionFrame))
+        gear_menu.add_command(label = "Furnishings", command = lambda: self.show_frame(FurnishingsFrame))
+        gear_menu.add_command(label = "Groceries", command = lambda: self.show_frame(GroceriesFrame))
+        gear_menu.add_command(label = "Housing", command = lambda: self.show_frame(HousingFrame))
+        gear_menu.add_command(label = "Lifestyle", command = lambda: self.show_frame(LifestyleFrame))
+        gear_menu.add_command(label = "Medical", command = lambda: self.show_frame(MedicalFrame))
+        gear_menu.add_command(label = "Miscellaneous", command = lambda: self.show_frame(MiscGearFrame)) # for player-added items with no type
+        gear_menu.add_command(label = "Personal Electronics", command = lambda: self.show_frame(PersonalElectronicsFrame))
+        gear_menu.add_command(label = "Security", command = lambda: self.show_frame(SecurityFrame))
+        gear_menu.add_command(label = "Surveillance", command = lambda: self.show_frame(SurveillanceFrame))
+        gear_menu.add_command(label = "Vehicles", command = lambda: self.show_frame(VehiclesFrame))
         self.menu_bar.add_cascade(label = "Gear", menu = gear_menu)
 
 
         weapons_menu = Menu(self.menu_bar, tearoff = 0)
-        weapons_menu.add_command(label = "All", command = self.show_frame(AllWeaponsFrame))
-        weapons_menu.add_command(label = "Exotics", command = self.show_frame(ExoticsFrame))
-        weapons_menu.add_command(label = "Heavy Weapons", command = self.show_frame(HeavyWeaponsFrame))
-        weapons_menu.add_command(label = "Melee", command = self.show_frame(MeleeFrame))
-        weapons_menu.add_command(label = "Miscellaneous", command = self.show_frame(MiscWeaponsFrame))  # for player-added items with no type
-        weapons_menu.add_command(label = "Pistols", command = self.show_frame(PistolsFrame))
-        weapons_menu.add_command(label = "Rifles", command = self.show_frame(RiflesFrame))
-        weapons_menu.add_command(label = "Shotguns", command = self.show_frame(ShotgunsFrame))
-        weapons_menu.add_command(label = "SMGs", command = self.show_frame(SMGsFrame))
+        weapons_menu.add_command(label = "All", command = lambda: self.show_frame(AllWeaponsFrame))
+        weapons_menu.add_command(label = "Exotics", command = lambda: self.show_frame(ExoticsFrame))
+        weapons_menu.add_command(label = "Heavy Weapons", command = lambda: self.show_frame(HeavyWeaponsFrame))
+        weapons_menu.add_command(label = "Melee", command = lambda: self.show_frame(MeleeFrame))
+        weapons_menu.add_command(label = "Miscellaneous", command = lambda: self.show_frame(MiscWeaponsFrame))  # for player-added items with no type
+        weapons_menu.add_command(label = "Pistols", command = lambda: self.show_frame(PistolsFrame))
+        weapons_menu.add_command(label = "Rifles", command = lambda: self.show_frame(RiflesFrame))
+        weapons_menu.add_command(label = "Shotguns", command = lambda: self.show_frame(ShotgunsFrame))
+        weapons_menu.add_command(label = "SMGs", command = lambda: self.show_frame(SMGsFrame))
 
 
         # misc is for player-added items with no type
         self.menu_bar.add_cascade(label = "Weapons", menu = weapons_menu)
 
         armor_menu = Menu(self.menu_bar, tearoff = 0)
-        armor_menu.add_command(label = "All", command = self.show_frame(AllArmorFrame))
+        armor_menu.add_command(label = "All", command = lambda: self.show_frame(AllArmorFrame))
         self.menu_bar.add_cascade(label = "Armor", menu = armor_menu)
 
 
